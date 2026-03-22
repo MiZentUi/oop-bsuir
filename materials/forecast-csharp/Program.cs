cfa62672ddec43f49448a3c4bf4873077698964d
JFomit <tzaberejny@gmail.com>
2026-03-22 12:55:15 +0300

    chore: initial project scaffolding

---
+++ materials/forecast-csharp/Program.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "WeatherExampleAPI";
+    config.Title = "Weather Example API";
+    config.Version = "v1";
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing",
+    "Bracing",
+    "Chilly",
+    "Cool",
+    "Mild",
+    "Warm",
+    "Balmy",
+    "Hot",
+    "Sweltering",
+    "Scorching",
+};
+
+app.MapGroup("api/v1")
+    .MapGet(
+        "/weather",
+        Ok<WeatherForecast[]>
+        () =>
+        {
+            var forecast = Enumerable
+                .Range(1, 5)
+                .Select(index => new WeatherForecast(
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+                .ToArray();
+                // 109
+            return TypedResults.Ok(forecast);
+        }
+    )
+    .WithName("GetCurrentWeather")
+    .WithDisplayName("Get Current Weather")
+    .WithTags(["weather"])
+    .WithDescription("Returns current weather for given coordinates");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
